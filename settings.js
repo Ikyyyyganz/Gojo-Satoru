90808aeb1a77dae9aeac9f8c36474a114f26b38c
ikyyymd3@gmail.com <136601745+Ikyyyyganz@users.noreply.github.com>
2023-07-03 18:43:30 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,10 +19,10 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
+global.owner = ['6283137808331']
+global.premium = ['6283137808331']
+global.ownernomer = '6283137808331'
+global.ownername = 'iky'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
